8095fcd3fdaad73aa68d3ae07631f1c2638e0ffd
Sean <shansari@verizon.net>
2018-12-18 12:53:35 -0500

    update 2 12/18/18

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArry = new int[] { 1, 3, 6, 4, 1, 2 };
+            int result = solution(myArry);
+            Console.WriteLine("This is the result: {0}", result);
+            Console.ReadLine();
+        }
+        public static int solution (int [] A)
+        {
+            int result = 0;
+            Array.Sort(A);
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i++] != A[i]++) {
+                    result = A[i++];
+                    result++;
+                    return result;
+                }
+                   
+            }
+            return A[0];
+        }
+    }
+}
